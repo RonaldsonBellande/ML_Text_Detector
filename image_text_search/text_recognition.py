5c90e61c3452693fd46db6ba0d1d2a8f2023423b
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-08-29 15:07:19 -0400

    for now

--- image_text_search/text_recognition.py
+++ image_text_search/text_recognition.py
@@ -6,16 +6,21 @@ from PIL import Image
 import os
 
 
-
 class framing_images(object):
     def __init__(self, target_word):
         
+        # Color
+        self.color = (255,0,0)
 
         # Paths
         self.absolute_path = os.path.abspath(os.getcwd()) 
+        self.absolute_path = os.listdir(self.absolute_path)
 
         # Images input
-
+        self.images = [count for count in glob(self.image_path +'*') if 'jpg' in count]
+        
+        # Image copy
+        self.image_copy = self.images
 
         # Target word in the text
         self.target_word = target_word
@@ -26,7 +31,29 @@ class framing_images(object):
         # Get all occurences of the that word
         self.word_occurences = [ i for i, word in enumerate(self.data["text"]) if word.lower() == self.target_word]
 
+    
 
+    def framing(self):
+        
+        for occ in word_occurences:
+
+            # Extract the width, height, top and left position for word wanted to detect in image
+            width = self.data["width"][occ]
+            height = self.data["height"][occ]
+            left = self.data["left"][occ]
+            top = self.data["top"][occ]
+            
+            # Surrounding points aroundthe world
+            point_1 = (left, top)
+            point_2 = (left + width, top)
+            point_3 = (left + width, top + height)
+            point_4 = (left, top + height)
+    
+            # Draw rectangle around the image if wanted
+            self.image_copy = cv2.line(self.image_copy, point_1, point_2, color=self.color , thickness=2)
+            self.image_copy = cv2.line(self.image_copy, point_2, point_3, color=self.color , thickness=2)
+            self.image_copy = cv2.line(self.image_copy, point_3, point_4, color=self.color , thickness=2)
+            self.image_copy = cv2.line(self.image_copy, point_4, point_1, color=self.color , thickness=2)
 
 
 
