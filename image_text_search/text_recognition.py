82b92a6ca64a6b8d5c5556f382d0abf693b127c3
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-08-28 21:40:26 -0400

    all

---
+++ image_text_search/text_recognition.py
@@ -0,0 +1,25 @@
+import pytesseract
+import cv2
+import sys
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+
+class framing_images(object):
+    def __init__(self):
+
+
+
+
+
+class recognize_image(object):
+    def __init__(self):
+
+
+
+
+class search_image(object):
+    def __init__(self):
+
+
