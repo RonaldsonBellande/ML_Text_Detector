4fcc253205419fa9d27cdea46e9437d86d89b693
RonaldsonBellande <ronaldsonbellande@gmail.com>
2021-08-29 05:02:17 -0400

    ok

--- image_text_search/text_recognition.py
+++ image_text_search/text_recognition.py
@@ -3,11 +3,28 @@ import cv2
 import sys
 import matplotlib.pyplot as plt
 from PIL import Image
+import os
 
 
 
 class framing_images(object):
-    def __init__(self):
+    def __init__(self, target_word):
+        
+
+        # Paths
+        self.absolute_path = os.path.abspath(os.getcwd()) 
+
+        # Images input
+
+
+        # Target word in the text
+        self.target_word = target_word
+
+        # Get all data from the image
+        self.data = pytesseract.image_to_data(self.image, output_type=pytesseract.Output.DICT)
+
+        # Get all occurences of the that word
+        self.word_occurences = [ i for i, word in enumerate(self.data["text"]) if word.lower() == self.target_word]
 
 
 
